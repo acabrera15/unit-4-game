04381532cd7932622ee87eb2a4e42f3f5784fb3c
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-04 16:58:32 -0700

    created objects for the characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var obi_wan_kenobi = {
+    healthPoints: 0,
+    attackPower: 0,
+    counterAttackPower: 0
+}
+
+var luke_skywalker = {
+    healthPoints: 0,
+    attackPower: 0,
+    counterAttackPower: 0
+}
+
+var darth_sidous = {
+    healthPoints: 0,
+    attackPower: 0,
+    counterAttackPower: 0
+}
+
+var darth_maul = {
+    healthPoints: 0,
+    attackPower: 0,
+    counterAttackPower: 0
+}
