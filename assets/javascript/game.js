26274429ec101d7d78f063541999958676b34ef4
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-04 17:22:14 -0700

    added a background image to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 var obi_wan_kenobi = {
     healthPoints: 0,
     attackPower: 0,
-    counterAttackPower: 0
+    counterAttackPower: 0,
+    imgAddress: '../images/obiWan'
 }
 
 var luke_skywalker = {
