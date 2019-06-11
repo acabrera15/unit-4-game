cddee229cdcfacb661b1222b2db37d9962228590
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-10 19:59:36 -0700

    added the functionality so that clinking chars works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,45 @@
 var obi_wan_kenobi = {
-    healthPoints: 0,
-    attackPower: 0,
-    counterAttackPower: 0,
-    imgAddress: '../images/obiWan'
-}
+  healthPoints: 0,
+  attackPower: 0,
+  counterAttackPower: 0,
+  imgAddress: "../images/obiWan"
+};
 
 var luke_skywalker = {
-    healthPoints: 0,
-    attackPower: 0,
-    counterAttackPower: 0
-}
+  healthPoints: 0,
+  attackPower: 0,
+  counterAttackPower: 0
+};
 
 var darth_sidous = {
-    healthPoints: 0,
-    attackPower: 0,
-    counterAttackPower: 0
-}
+  healthPoints: 0,
+  attackPower: 0,
+  counterAttackPower: 0
+};
 
 var darth_maul = {
-    healthPoints: 0,
-    attackPower: 0,
-    counterAttackPower: 0
-}
+  healthPoints: 0,
+  attackPower: 0,
+  counterAttackPower: 0
+};
 
+$(document).ready(function() {
+  var initialCharSelected = false;
+
+  $(".card").on("click", function(e) {
+    var cardArr = $(".card");
+
+    if (!initialCharSelected) {
+      $("#" + e.currentTarget.id).appendTo(".yourChar");
+      initialCharSelected = true;
+        var counter = 1;
+      for (var i = 0; i < cardArr.length; i++) {
+        console.log(cardArr[i].id);
+        if (e.currentTarget.id != cardArr[i].id) {
+            $('#' + cardArr[i].id).appendTo('#attack' + counter.toString());
+            counter++;
+        }
+      }
+    }
+  });
+});
