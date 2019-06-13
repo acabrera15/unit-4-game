2d7d81319feda5a81a87fc7f706b2acac88c3dd8
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-12 20:33:37 -0700

    added the text ouput of the fight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,28 @@ var obi_wan_kenobi = {
   healthPoints: 120,
   attackPower: 8,
   counterAttackPower: 10,
-  name: 'obi_wan_kenobi'
+  name: "obi_wan_kenobi",
 };
 
 var lukeSkywalker = {
   healthPoints: 180,
   attackPower: 8,
   counterAttackPower: 5,
-  name: 'lukeSkywalker'
+  name: "lukeSkywalker"
 };
 
 var darthSidious = {
   healthPoints: 150,
   attackPower: 8,
   counterAttackPower: 20,
-  name: 'darthSidious'
+  name: "darthSidious"
 };
 
 var darthMaul = {
   healthPoints: 180,
   attackPower: 10,
   counterAttackPower: 25,
-  name: 'darthMaul'
+  name: "darthMaul"
 };
 
 //sets the healthPoints to the display
@@ -34,7 +34,7 @@ var setHealthOnDisplay = function() {
   $("#darthMaulHealthPoints").text(darthMaul.healthPoints);
 };
 
-var setObjectVarFromId = function (id) {
+var setObjectVarFromId = function(id) {
   if (id === obi_wan_kenobi.name) {
     return obi_wan_kenobi;
   } else if (id === lukeSkywalker.name) {
@@ -44,7 +44,7 @@ var setObjectVarFromId = function (id) {
   } else if (id === darthMaul.name) {
     return darthMaul;
   } else {
-    console.log('unknown');
+    console.log("unknown");
   }
 };
 
@@ -53,6 +53,7 @@ $(document).ready(function() {
   var currentCharDiv;
   var characterChosen;
   var characterToFight;
+  var attackPowerHolder = 0;
 
   setHealthOnDisplay();
 
@@ -64,6 +65,7 @@ $(document).ready(function() {
     if (!initialCharSelected) {
       currentCharDiv = e.currentTarget.id; //hold current character
       characterChosen = setObjectVarFromId(e.currentTarget.id); //holds the chosen character
+      attackPowerHolder = characterChosen.attackPower;
 
       $("#" + currentCharDiv).appendTo(".yourChar");
       initialCharSelected = true;
@@ -81,13 +83,26 @@ $(document).ready(function() {
     }
   });
 
-  $("#attackButton").on('click', function () {
+  $("#attackButton").on("click", function() {
     if (initialCharSelected && characterToFight !== undefined) {
+
+      $("#youAttackP").text(
+        `You attacked ${characterToFight.name} for ${
+          characterChosen.attackPower
+        } damage`
+      );
+
+      $("#defenderAttackedP").text(
+        `${characterToFight.name} attacked you back for ${
+          characterToFight.counterAttackPower
+        } damage`
+      );
+
       characterChosen.healthPoints -= characterToFight.counterAttackPower;
-      setHealthOnDisplay(); 
+      characterToFight.healthPoints -= characterChosen.attackPower;
+      characterChosen.attackPower += attackPowerHolder;
+
+      setHealthOnDisplay();
     }
   });
-
-
-
 });
