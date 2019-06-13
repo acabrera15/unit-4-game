e490939aa15b3963100d8a60e5df05bbe566dd84
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-12 19:47:13 -0700

    added functionality so that the healthpoints get lowered

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,31 @@ var obi_wan_kenobi = {
   healthPoints: 120,
   attackPower: 8,
   counterAttackPower: 10,
-  imgAddress: "../images/obiWan"
+  name: 'obi_wan_kenobi'
 };
 
 var lukeSkywalker = {
   healthPoints: 180,
   attackPower: 8,
-  counterAttackPower: 5
+  counterAttackPower: 5,
+  name: 'lukeSkywalker'
 };
 
 var darthSidious = {
   healthPoints: 150,
   attackPower: 8,
-  counterAttackPower: 20
+  counterAttackPower: 20,
+  name: 'darthSidious'
 };
 
 var darthMaul = {
   healthPoints: 180,
   attackPower: 10,
-  counterAttackPower: 25
+  counterAttackPower: 25,
+  name: 'darthMaul'
 };
 
+//sets the healthPoints to the display
 var setHealthOnDisplay = function() {
   $("#obi_wan_kenobiHealthPoints").text(obi_wan_kenobi.healthPoints);
   $("#lukeSkywalkerHealthPoints").text(lukeSkywalker.healthPoints);
@@ -30,18 +34,36 @@ var setHealthOnDisplay = function() {
   $("#darthMaulHealthPoints").text(darthMaul.healthPoints);
 };
 
+var setObjectVarFromId = function (id) {
+  if (id === obi_wan_kenobi.name) {
+    return obi_wan_kenobi;
+  } else if (id === lukeSkywalker.name) {
+    return lukeSkywalker;
+  } else if (id === darthSidious.name) {
+    return darthSidious;
+  } else if (id === darthMaul.name) {
+    return darthMaul;
+  } else {
+    console.log('unknown');
+  }
+};
+
 $(document).ready(function() {
   var initialCharSelected = false; //if character is selected
   var currentCharDiv;
+  var characterChosen;
+  var characterToFight;
 
   setHealthOnDisplay();
 
   //do something if a card is clicked
   $(".card").on("click", function(e) {
     var cardArr = $(".card"); //array of the different cards
+
     //cheks if character isnt selected
     if (!initialCharSelected) {
       currentCharDiv = e.currentTarget.id; //hold current character
+      characterChosen = setObjectVarFromId(e.currentTarget.id); //holds the chosen character
 
       $("#" + currentCharDiv).appendTo(".yourChar");
       initialCharSelected = true;
@@ -55,6 +77,17 @@ $(document).ready(function() {
       }
     } else if (e.currentTarget.id != currentCharDiv) {
       $("#" + e.currentTarget.id).appendTo("#defenderSection");
+      characterToFight = setObjectVarFromId(e.currentTarget.id);
     }
   });
+
+  $("#attackButton").on('click', function () {
+    if (initialCharSelected && characterToFight !== undefined) {
+      characterChosen.healthPoints -= characterToFight.counterAttackPower;
+      setHealthOnDisplay(); 
+    }
+  });
+
+
+
 });
